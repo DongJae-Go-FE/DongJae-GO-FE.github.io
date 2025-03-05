7ef57c9bf38dfbfb1221d1ed3bc1facea89c7026
가동재 <masterforce999@naver.com>
2025-03-05 16:23:37 +0900

    배포 테스트

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1741158997',resources: [
+    cacheName: 'chirpy-1741159403',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
